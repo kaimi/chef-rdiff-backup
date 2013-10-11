bc29c98dc0513434bd9a801f5834bd3e76431434
ka’imi <kaimi@piratenfraktion-nrw.de>
2013-10-11 15:31:21 +0200

    added autotrim capabilities

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,3 +11,6 @@ default['rdiff-backup']['var_dir'] = "/var/rdiff-backup"
 default['rdiff-backup']['user'] = "rdiff-backup"
 default['rdiff-backup']['key'] = "id_rsa"
 default['rdiff-backup']['notification_email'] = "it@piratenfraktion-nrw.de"
+
+default['rdiff-backup']['autotrim_enable'] = false
+default['rdiff-backup']['autotrim_timespan'] = "1y"
