ec8d0c8506c0df9388098605ea9fad31c69d159f
ka’imi <kaimi@piratenfraktion-nrw.de>
2013-10-11 15:41:09 +0200

    removed unused `var_dir` attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,6 @@
 
 default['rdiff-backup']['backup_dir'] = "/backup"
 default['rdiff-backup']['etc_dir'] = "/etc/rdiff-backup"
-default['rdiff-backup']['var_dir'] = "/var/rdiff-backup"
 default['rdiff-backup']['user'] = "rdiff-backup"
 default['rdiff-backup']['key'] = "id_rsa"
 default['rdiff-backup']['notification_email'] = "it@piratenfraktion-nrw.de"
